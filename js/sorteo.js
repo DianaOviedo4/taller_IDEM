12c6dedb26abf0f8b1ea451e8af460cb873796c4
DianaOviedo4 <dianamarcelaoviedo76@gmail.com>
2021-07-23 16:52:42 -0500

    haciendo el reto jeje

--- js/sorteo.js
+++ js/sorteo.js
@@ -4,9 +4,20 @@ let correos = ["diana@gmail.com", "seas@gmail.com", "lupita@gmail.com", "juan@gm
 console.log(correos.length);
 
 let ganador = document.getElementById('botoncito');
-ganador.addEventListener('click', function () {
-    console.log('holi');
-    var resp = correos[Math.floor(Math.random()*correos.length)];
+ganador.addEventListener('submit', getWinner());
+
+
+function getWinner() {
+    console.log('toy dentro');
+    var resp = correos[Math.floor(Math.random() * correos.length)]; // nos aseguramos de la longitud de la matriz
     console.log(resp);
-    
-});
+
+    let view = document.getElementById('resultado');
+    view.innerText = resp;
+
+};
+
+let p = setInterval('getWinner()', 200);
+setTimeout(() => {
+    clearInterval(p); console.log('FINAAAAL');
+}, 5000);
